8c692c23e3af5624240b5891b5a663ed589bcc56
Daniil <dtuneev@gmail.com>
2022-05-08 22:37:23 +0300

    user model + npm modules + authentication

---
+++ models/user.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+  googleId: {
+    type: String,
+    required: true
+  },
+  displayName: {
+    type: String,
+    default: false
+  },
+  firstName: {
+    type: String,
+    default: false
+  },
+  lastName: {
+    type: String,
+    default: false
+  },
+  image: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
+module.exports = mongoose.model('User', UserSchema)
--- server.js
+++ server.js
@@ -3,16 +3,32 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const passport = require('passport')
+const session = require('express-session')
 
-mongoose.connect(process.env.DATABA_URL, { useNewUrlParser: true })
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
-db.once('open', () => console.error('Connected to Database'))
+db.once('open', () =>  console.error('Connected to Database'))
+
+// Passport config
+require('./config/passport')(passport)
+
+// Session middleware *above passport*
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: true }
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use(express.json())
 
 const todoshhhRouter = require('./routes/todoshhh')
 app.use('/todoshhh', todoshhhRouter)
 
-app.listen(3000, () => console.log('Server Started'))
-
+app.listen(process.env.PORT, () => console.log('Server Started'))
