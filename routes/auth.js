8d8083daeeb5111686811f101531ad92e63cc91b
Daniil <dtuneev@gmail.com>
2022-08-28 23:53:10 +0300

    auth methods

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const passport = require('passport')
 const router = express.Router()
 const { ensureAuth } = require('../middleware/auth')
-const TodoItem = require('../models/todo_item')
+const UserItem = require('../models/user')
 
 // @desc    Auth with Google
 // @route   GET /auth/google
@@ -10,26 +10,31 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 // @desc    Google auth callback
 // @route   GET /auth/google/callback
-router.get(
-  '/google/callback',
-  passport.authenticate('google', { failureRedirect: '/' }),
+router.get('/google/callback',
+  passport.authenticate('google', { failureRedirect: process.env.FRONTEND_URL }),
   (req, res) => {
-    res.redirect(301, process.env.FRONTEND_URL)
+    res.status(301).redirect(process.env.FRONTEND_URL)
   }
 )
 
 // @desc    Google auth callback
 // @route   GET /auth/google/callback
 router.get('/me', ensureAuth,  (req, res) => {
-    res.json(req.user);
-  }
-)
+  res.json(req.user);
+})
 
 // @desc    Logout user
 // @route   /auth/logout
-router.get('/logout', (req, res) => {
-  req.logout()
-  res.redirect('/')
-})
+router.get('/logout', function(req, res){
+  req.logout();
+  res.json({ message: 'you successfully logged out' });
+});
+
+// @desc    Logout user
+// @route   /auth/logout
+router.get('/logout2', function(req, res){
+  req.logout();
+  res.redirect(process.env.FRONTEND_URL);
+});
 
 module.exports = router
